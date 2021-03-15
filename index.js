59de3b70938fa345cfd762dfc249a62bbc9b63d9
Stacey McKnight <mcknight.stm@gmail.com>
2021-03-15 10:48:37 -0500

    commit tree 3929938f2cb126e92ae0944141c961163996f5a3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.getElementById('main').remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.appendChild(newHeader)
+
+
+
+
+
